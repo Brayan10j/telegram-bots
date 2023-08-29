8966b15d64aa9210735db421068e3fbc87f604d4
Brayan <brayanespitia10@gmail.com>
2023-08-29 10:10:40 -0500

    launch

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on("text", (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch().then(() => {
-    console.log('Bot iniciado');
-}).catch((err) => {
-    console.error('Error al iniciar el bot', err);
-});
+bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
