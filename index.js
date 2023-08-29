bed14b9bb0c8838fa482404e715b9e9762a88434
Brayan <brayanespitia10@gmail.com>
2023-08-29 12:11:16 -0500

    only express

--- index.js
+++ index.js
@@ -9,39 +9,31 @@ Human:
 {input}
 AI:`); */
 
-
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-  })
-
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST request to the homepage')
-})
 
 const bot = new Telegraf(process.env.BOT);
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("text", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-if (process.env.NODE_ENV == "production") {
-    app.use(await bot.createWebhook({ domain: 'https://siontelegram-bots-352013a47d20.herokuapp.com' }));
+
+bot.launch();
+/* if (process.env.NODE_ENV == "production") {
+  app.use(
+    await bot.createWebhook({
+      domain: "https://siontelegram-bots-352013a47d20.herokuapp.com",
+    })
+  );
 } else {
   // if local use Long-polling
   bot.launch().then(() => {
     console.info(`The bot ${bot.botInfo.username} is running locally`);
   });
-}
+} */
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-
-
-
-
-app.listen(process.env.PORT || 3000 , () => console.log("Listening server"));
+app.listen(process.env.PORT || 3000, () => console.log("Listening server"));
